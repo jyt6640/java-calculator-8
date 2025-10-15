8797d47c968c2ee6261a9dbf02802eaaa1d1c497
jyt6640 <jyt6640@gmail.com>
2025-10-15 13:14:08 +0900

    test: 숫자 하나 입력 테스트 추가

--- src/test/java/calculator/StringCalculatorTest.java
+++ src/test/java/calculator/StringCalculatorTest.java
@@ -23,4 +23,17 @@ public class StringCalculatorTest {
         //then
         assertEquals(0, result);
     }
+
+    @DisplayName("숫자 하나 입력 시 숫자 하나만 반환")
+    @Test
+    void 숫자_하나_입력_시_숫자_하나만_반환() {
+        //given
+        String input = "1";
+
+        //when
+        int result = StringCalculator.calculate(input);
+
+        //then
+        assertEquals(1, result);
+    }
 }
