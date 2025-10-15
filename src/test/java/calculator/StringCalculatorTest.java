c8dd1662685cf201009d99ef6f453823f9409b36
jyt6640 <jyt6640@gmail.com>
2025-10-15 13:02:52 +0900

    test: 빈 문자열 입력 시 0 반환 테스트 추가

---
+++ src/test/java/calculator/StringCalculatorTest.java
@@ -0,0 +1,27 @@
+package calculator;
+
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+
+public class StringCalculatorTest {
+
+    @DisplayName("빈 문자열은 0 반환")
+    @Test
+    void 빈_문자열은_0을_반환() {
+        //given
+        String input = "";
+
+        //when
+        int result = StringCalculator.calculate(input);
+
+        //then
+        assertEquals(0, result);
+    }
+
+}
