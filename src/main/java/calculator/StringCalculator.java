90b2782452268ffe9d7795761a4c7546fc5d6653
jyt6640 <jyt6640@gmail.com>
2025-10-15 17:54:16 +0900

    feat: 커스텀 구분자 추출 기능 구현

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -2,8 +2,21 @@ package calculator;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class StringCalculator {
+
+    private static final Pattern CUSTOM_DELIMITER_PATTERN = Pattern.compile("^//(.+?)\\n");
+
+    public static String customDelimiter(String input) {
+        Matcher matcher = CUSTOM_DELIMITER_PATTERN.matcher(input);
+        if (matcher.find()) {
+            return matcher.group(1);
+        }
+        return null;
+    }
+
     public static int calculate(String input) {
         if (input.isEmpty()) return 0;
 
@@ -24,5 +37,4 @@ public class StringCalculator {
 
         return sum;
     }
-
 }
