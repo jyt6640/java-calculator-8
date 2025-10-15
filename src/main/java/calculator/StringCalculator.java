a002ccb26f686a9913c3ea65d240a3ccfb6b920d
jyt6640 <jyt6640@gmail.com>
2025-10-15 13:15:02 +0900

    feat: 숫자 하나 입력 기능 구현

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -3,7 +3,10 @@ package calculator;
 public class StringCalculator {
     public static int calculate(String input) {
         if (input == "") return 0;
-        return 0;
+
+        int result = Integer.parseInt(input);
+
+        return result;
     }
 
 }
