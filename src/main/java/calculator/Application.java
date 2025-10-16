d8e3b457aa8c85cd112ec7c463e07c5e014b40db
jyt6640 <jyt6640@gmail.com>
2025-10-16 14:18:41 +0900

    feat: 예외 처리 메시지 출력 기능 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -6,7 +6,15 @@ public class Application {
     public static void main(String[] args) {
         System.out.println("덧셈할 문자열을 입력해 주세요");
         String input = Console.readLine();
-        int result = StringCalculator.calculate(input);
-        System.out.println("결과 : " + result);
+        try {
+            int result = StringCalculator.calculate(input);
+            System.out.println("결과 : " + result);
+        } catch (IllegalArgumentException e) {
+            System.out.println("[ERROR] : " + e.getMessage());
+        } catch (Exception e) {
+            System.out.println("[ERROR] : 알 수 없는 오류가 발생했습니다.");
+        } finally {
+            Console.close();
+        }
     }
 }
