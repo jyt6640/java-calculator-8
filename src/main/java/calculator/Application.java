ca422ba8b26ec931834f8f336717cc07f06ed871
jyt6640 <jyt6640@gmail.com>
2025-10-16 15:44:09 +0900

    refactor: 입력 기능 InputView로 분리

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -4,8 +4,7 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
-        System.out.println("덧셈할 문자열을 입력해 주세요");
-        String input = Console.readLine();
+        String input = InputView.readInput();
         try {
             int result = StringCalculator.calculate(input);
             System.out.println("결과 : " + result);
