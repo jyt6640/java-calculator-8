6d0e5dc34a4e9aba9ecd5f87a0008c33cde42eed
jyt6640 <jyt6640@gmail.com>
2025-10-16 13:24:09 +0900

    test: 예외 발생 테스트 추가

--- src/test/java/calculator/StringCalculatorTest.java
+++ src/test/java/calculator/StringCalculatorTest.java
@@ -1,6 +1,5 @@
 package calculator;
 
-import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -114,5 +113,46 @@ public class StringCalculatorTest {
 
         //then
         assertEquals(6, result);
+    }
+
+    //------------------------------예외 테스트------------------------------
+    @DisplayName("음수 입력 예외 발생")
+    @Test
+    void 음수_입력_예외_발생() {
+        //given
+        String input = "-1,2,3";
+
+        //when&then
+        assertThrows(IllegalArgumentException.class, () -> StringCalculator.calculate(input));
+    }
+
+    @DisplayName("숫자가 아닌 값 입력 시 예외 발생")
+    @Test
+    void 숫자가_아닌_값_입력_시_예외_발생() {
+        //given
+        String input = "1,a,3";
+
+        //when&then
+        assertThrows(IllegalArgumentException.class, () -> StringCalculator.calculate(input));
+    }
+
+    @DisplayName("커스텀 구분자 누락 시 예외 발생")
+    @Test
+    void 커스텀_구분자_누락_시_예외_발생 () {
+        //given
+        String input = "//\n1;2;3";
+
+        //when&then
+        assertThrows(IllegalArgumentException.class, () -> StringCalculator.extractCustomDelimiter(input));
+    }
+
+    @DisplayName("개행 표식 누락 시 예외 발생")
+    @Test
+    void 개행_표식_누락_시_예외_발생() {
+        //given
+        String input = "//;1;2;3";
+
+        //when&then
+        assertThrows(IllegalArgumentException.class, () -> StringCalculator.extractNumbersPart(input));
     }
 }
