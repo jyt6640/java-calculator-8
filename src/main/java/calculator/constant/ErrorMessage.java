7a9aceff45b8d5ea8844919280f3ac1c3a88ed70
jyt6640 <jyt6640@gmail.com>
2025-10-17 22:10:56 +0900

    feat: 커스텀 구분자 검증 강화 및 오류 메시지 상수 추가

--- src/main/java/calculator/constant/ErrorMessage.java
+++ src/main/java/calculator/constant/ErrorMessage.java
@@ -4,7 +4,8 @@ public enum ErrorMessage {
     NEGATIVE_NUMBER("음수는 허용되지 않습니다."),
     NOT_A_NUMBER("숫자가 아닌 값이 포함되어 있습니다."),
     EMPTY_CUSTOM_DELIMITER("커스텀 구분자가 비어있습니다."),
-    INVALID_CUSTOM_DELIMITER_FORMAT("커스텀 구분자 형식이 올바르지 않습니다.");
+    INVALID_CUSTOM_DELIMITER_FORMAT("커스텀 구분자 형식이 올바르지 않습니다."),
+    INVALID_DELIMITER_USAGE("선언된 구분자 이외의 구분자가 입력되었습니다.");
 
     private static final String PREFIX = "[ERROR] ";
     private final String message;
