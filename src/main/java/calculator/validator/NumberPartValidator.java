6e48bf07ed1c8745c7a94019fe46a79685372a21
jyt6640 <jyt6640@gmail.com>
2025-10-17 22:09:12 +0900

    feat: 선언된 구분자 이외의 구분자 사용시 예외 발생 기능 구현

--- src/main/java/calculator/validator/NumberPartValidator.java
+++ src/main/java/calculator/validator/NumberPartValidator.java
@@ -1,10 +1,16 @@
 package calculator.validator;
 
+import static calculator.constant.Patterns.CUSTOM_DELIMITER;
+import static calculator.constant.Patterns.INVALID_CHAR_PATTERN;
 import static calculator.constant.Patterns.ONLY_NUMBER;
 
 import calculator.constant.ErrorMessage;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class NumberPartValidator {
+    private static final Pattern CUSTOM_DELIMITER_PATTERN = Pattern.compile(CUSTOM_DELIMITER);
+
     public boolean hasNegativeNumberValidate(String[] tokens) {
         for (String token : tokens) {
             int number = Integer.parseInt(token);
@@ -24,4 +30,27 @@ public class NumberPartValidator {
         }
         return false;
     }
+
+    public void validateWithoutCustomDelimiter(String input) {
+        if(input.matches(INVALID_CHAR_PATTERN)) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_DELIMITER_USAGE.getMessage());
+        }
+    }
+
+    public void validateWithCustomDelimiter(String input) {
+        Matcher matcher = CUSTOM_DELIMITER_PATTERN.matcher(input);
+
+        if (!matcher.find()) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_CUSTOM_DELIMITER_FORMAT.getMessage());
+        }
+
+        String customDelimiter = matcher.group(1);
+        String numbersPart = input.substring(matcher.end());
+        String allowedPattern = "^[0-9,:" + Pattern.quote(customDelimiter) + "\\n]+$";
+
+        if (!numbersPart.matches(allowedPattern)) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_DELIMITER_USAGE.getMessage());
+        }
+    }
+
 }
