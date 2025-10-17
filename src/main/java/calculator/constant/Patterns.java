fe3ec3b968c005bf8890d25047514eb59638bc6a
jyt6640 <jyt6640@gmail.com>
2025-10-17 22:38:47 +0900

    fix: 양수만 받던 상수를 음수도 같이 받게 정규식 변경

--- src/main/java/calculator/constant/Patterns.java
+++ src/main/java/calculator/constant/Patterns.java
@@ -2,7 +2,7 @@ package calculator.constant;
 
 public class Patterns {
     public static final String CUSTOM_DELIMITER = "^//(.+?)\\\\n";
-    public static final String ONLY_NUMBER = "^\\d*$";
+    public static final String ONLY_NUMBER = "^-?\\d+$";
     public static final String INVALID_CHAR_PATTERN = ".*[^0-9,:\\n].*";
 
     private Patterns() {}
