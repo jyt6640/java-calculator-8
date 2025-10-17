5a45b5bd061589dc7b26829d74b0e8ba99dd186c
jyt6640 <jyt6640@gmail.com>
2025-10-17 20:19:41 +0900

    refactor: 숫자 문자열 추출 기능 제거 및 token 값 int로 변환 후 바로 sum으로 리턴

--- src/main/java/calculator/service/Calculator.java
+++ src/main/java/calculator/service/Calculator.java
@@ -1,20 +1,14 @@
 package calculator.service;
 
-import calculator.validator.NumberPartValidator;
-
 public class Calculator {
-
-    private final NumberPartValidator numberPartValidator = new NumberPartValidator();
-
     public int calculate(String[] tokens) {
         int sum = 0;
         for (String token : tokens) {
             if (token.isEmpty()) {
                 continue;
             }
-            int number = numberPartValidator.parseAndValidateNumber(token);
 
-            sum += number;
+            sum += Integer.parseInt(token);
         }
 
         return sum;
