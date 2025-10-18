6d445df2d6a6a477e95b7997abe02ae8c50a49cd
jyt6640 <jyt6640@gmail.com>
2025-10-18 10:37:09 +0900

    test: 일급 컬렉션에서 숫자 배열 합계산 테스트 추가

---
+++ src/test/java/calculator/domain/NumbersTest.java
@@ -0,0 +1,22 @@
+package calculator.domain;
+
+import org.assertj.core.internal.Numbers;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class NumbersTest {
+    @DisplayName("숫자 배열의 합 계산")
+    @Test
+    void 숫자_배열의_합_계산() {
+        //given
+        String[] tokens = {"3","5","7"};
+
+        //when
+        Numbers numbers = new Numbers(tokens);
+        int result = numbers.sum();
+
+        //then
+        Assertions.assertEquals(15, result);
+    }
+}
