e8163d6894a64a200609027bbe3cd5dcb8079970
jyt6640 <jyt6640@gmail.com>
2025-10-18 10:15:06 +0900

    refactor: 음수 입력 검증 도메인에서 검증 하도록 변환

--- src/main/java/calculator/domain/Number.java
+++ src/main/java/calculator/domain/Number.java
@@ -1,10 +1,20 @@
 package calculator.domain;
 
+import calculator.constant.ErrorMessage;
+
 public class Number {
     private final int value;
 
     public Number(String token) {
         this.value = Integer.parseInt(token);
+
+        validateNegative(this.value);
+    }
+
+    private void validateNegative(int number) {
+        if (value < 0 ) {
+            throw new IllegalArgumentException(ErrorMessage.NEGATIVE_NUMBER.getMessage());
+        }
     }
 
     public int getValue() {
