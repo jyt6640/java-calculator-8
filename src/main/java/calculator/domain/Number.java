a8b36ba76fc615fa001dca14164621b1ab99df06
jyt6640 <jyt6640@gmail.com>
2025-10-18 10:22:18 +0900

    feat: Number에 빈 입력값 입력 시 0 반환 기능 구현

--- src/main/java/calculator/domain/Number.java
+++ src/main/java/calculator/domain/Number.java
@@ -6,15 +6,26 @@ public class Number {
     private final int value;
 
     public Number(String token) {
+        token = normalizeEmptyInput(token);
+
         this.value = Integer.parseInt(token);
 
         validateNegative(this.value);
+
+
     }
 
     private void validateNegative(int number) {
         if (value < 0 ) {
             throw new IllegalArgumentException(ErrorMessage.NEGATIVE_NUMBER.getMessage());
         }
+    }
+
+    private String normalizeEmptyInput(String token) {
+        if (token == null || token.isEmpty()) {
+            return "0";
+        }
+        return token;
     }
 
     public int getValue() {
