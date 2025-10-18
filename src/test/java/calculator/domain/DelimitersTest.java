9d3367173d667ccd86b8fee99afa35b3b09e3522
jyt6640 <jyt6640@gmail.com>
2025-10-18 14:45:52 +0900

    test: 일급 컬렉션으로 기본 구분자 집합 정의 테스트 추가

---
+++ src/test/java/calculator/domain/DelimitersTest.java
@@ -0,0 +1,25 @@
+package calculator.domain;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Set;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class DelimitersTest {
+    @DisplayName("기본 구분자 집합 정의")
+    @Test
+    void 기본_구분자_집합_정의() {
+        //given
+        Delimiters delimiters = new Delimiters(null);
+
+        //when
+        Set<String> result = delimiters.getDelimiters();
+
+        //then
+        assertEquals(2, result.size());
+        assertTrue(result.contains(","));
+        assertTrue(result.contains(":"));
+    }
+}
