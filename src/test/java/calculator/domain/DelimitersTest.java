13941b90a7600ef8e3565bda8c4eaa4563dd427b
jyt6640 <jyt6640@gmail.com>
2025-10-18 16:44:03 +0900

    test: 집합 정의 관련 테스트 제거 - 기본 구분자 및 커스텀 구분자 정의 테스트 제거 - 구분자로 문자열 분리 시 집합 정의가 이행 됨

--- src/test/java/calculator/domain/DelimitersTest.java
+++ src/test/java/calculator/domain/DelimitersTest.java
@@ -9,50 +9,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class DelimitersTest {
-    @DisplayName("기본 구분자 집합 정의")
-    @Test
-    void 기본_구분자_집합_정의() {
-        //given
-        Delimiters delimiters = new Delimiters(null);
-
-        //when
-        Set<String> result = delimiters.getDelimiters();
-
-        //then
-        assertEquals(2, result.size());
-        assertTrue(result.contains(","));
-        assertTrue(result.contains(":"));
-    }
-
-    @DisplayName("커스텀 구분자 추가")
-    @Test
-    void 커스텀_구분자_추가() {
-        //given
-        Delimiters delimiters = new Delimiters("#");
-
-        //when
-        Set<String> result = delimiters.getDelimiters();
-
-        //then
-        assertEquals(3, result.size());
-        assertTrue(result.contains("#"));
-        assertTrue(result.contains(":"));
-        assertTrue(result.contains(","));
-    }
-
-    @DisplayName("중복 구분자 제거")
-    @Test
-    void 중복_구분자_제거() {
-        //given
-        Delimiters delimiters = new Delimiters(",");
-
-        //when
-        Set<String> result = delimiters.getDelimiters();
-
-        //then
-        assertEquals(2, result.size());
-    }
-
     @DisplayName("기본 구분자로 문자열 분리")
     @Test
     void 기본_구분자로_문자열_분리() {
