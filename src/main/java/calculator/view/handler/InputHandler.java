96a19afe5ccb6b3eff109aac332f9ed31fb2a8ab
jyt6640 <jyt6640@gmail.com>
2025-12-31 14:10:53 +0900

    feat: 최소한의 예외 처리 기능 구현

--- src/main/java/calculator/view/handler/InputHandler.java
+++ src/main/java/calculator/view/handler/InputHandler.java
@@ -6,6 +6,7 @@ import java.util.regex.Pattern;
 
 public class InputHandler {
     private static final Pattern CUSTOM_DELIMITER_PATTERN = Pattern.compile("^//(.*?)\\\\n");
+    private static final Pattern VALID_INPUT_PATTERN = Pattern.compile("^[0-9,;]+$");
 
     private final InputView inputView;
 
@@ -14,14 +15,17 @@ public class InputHandler {
     }
 
     public String getNumber() {
-        return inputView.readNumber();
+        String input = inputView.readNumber();
+        validateEmpty(input);
+        return input;
     }
 
     public boolean hasCustomDelimiter(String input) {
-        Matcher matcher = CUSTOM_DELIMITER_PATTERN.matcher(input);
-        if (matcher.find()) {
+        if (input.startsWith("//")) {
+            validateRegex(input);
             return true;
         }
+        validateBasicInput(input);
         return false;
     }
 
@@ -39,6 +43,29 @@ public class InputHandler {
             return matcher.group(1);
         }
         return null;
+    }
+
+    private void validateEmpty(String numbers) {
+        if (numbers.isBlank()) {
+            throw new IllegalArgumentException("[ERROR] 입력 값이 공백입니다.");
+        }
+    }
+
+    private void validateRegex(String input) {
+        Matcher matcher = CUSTOM_DELIMITER_PATTERN.matcher(input);
+        if (!matcher.find()) {
+            throw new IllegalArgumentException("[ERROR] 올바르지 않은 입력 형식입니다.");
+        }
+        String delimiter = matcher.group(1);
+        if (delimiter == null || delimiter.trim().isBlank()) {
+            throw new IllegalArgumentException("[ERROR] 커스텀 구분자가 공백입니다.");
+        }
+    }
+
+    private void validateBasicInput(String input) {
+        if (!VALID_INPUT_PATTERN.matcher(input).matches()) {
+            throw new IllegalArgumentException("[ERROR] 올바르지 않은 입력 형식입니다.");
+        }
     }
 
 }
